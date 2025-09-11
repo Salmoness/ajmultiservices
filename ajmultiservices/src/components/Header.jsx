c00fbfb0328a499516950983788c6208c0837caf
Saymon <slrivas24@gmail.com>
2025-09-11 16:52:45 -0400

    fix: made header look nicer

--- ajmultiservices/src/components/Header.jsx
+++ ajmultiservices/src/components/Header.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import { Button } from "./ui/button";
 
 export default function Header() {
     return (
         <header className="sticky top-0 z-51 bg-black/80 backdrop-blur-md px-6 py-8 flex justify-between items-center border-b border-gray-800">
             <h1 className="text-2xl font-bold tracking-widest">AJ MULTISERVICES</h1>
-            <nav className="space-x-6">
-                <a href="#" className="hover:text-gray-400">Home</a>
-                <a href="#" className="hover:text-gray-400">Gallery</a>
-                <a href="#" className="hover:text-gray-400">Contact</a>
+            <nav className="space-x-6 font-bold tracking-wider">
+                <Button href="#" variant="ghost" className="hover:text-gray-700">Home</Button>
+                <Button href="#" variant="ghost" className="hover:text-gray-700">Gallery</Button>
+                <Button href="#" variant="ghost" className="hover:text-gray-700">Contact</Button>
             </nav>
+            <Button variant="secondary" size="lg" className="cursor-pointer">Get a free quote</Button>
         </header>
     );
 }
